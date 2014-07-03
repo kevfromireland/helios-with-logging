3c5e1441a275a5e2e36445d767dde0cbaccb90dd
Kevin Boyle <kevin.boyle@outlook.com>
2014-07-03 11:56:03 +0100

    Sample project that shows you need to add the listeners to your web.config manually

---
+++ src/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Owin;
+
+namespace Helios.Logging.Example
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(async x =>
+                          {
+                              Trace.TraceInformation("Trace information message");
+
+                              var azureListenerAssemblies = Trace.Listeners.Cast<TraceListener>()
+                                  .Where(listener => listener.Name.Contains("Azure"))
+                                  .Select(listener => listener.GetType().Assembly)
+                                  .Select(assembly => assembly.CodeBase)
+                                  .ToList();
+
+                              x.Response.StatusCode = (int) HttpStatusCode.OK;
+
+                              byte[] readAllBytes = File.ReadAllBytes(new Uri(azureListenerAssemblies.First()).LocalPath);
+
+                              x.Response.ContentType = "application/octet-stream";
+                              await x.Response.WriteAsync(readAllBytes);
+
+
+                          });
+        }
+    }
+}
